ba48e40706fa064b9ac853ddacc56db6ba19ba15
mzl <you@example.com>
2016-06-20 18:00:53 +0800

    动态命名空间

--- TestSwiftOne/Classes/Main/MainViewController.swift
+++ TestSwiftOne/Classes/Main/MainViewController.swift
@@ -14,15 +14,28 @@ command + j -> 定位到目录结构
 command + n 创建文件
 */
 class MainViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tabBar.tintColor = UIColor.orangeColor()
         
-        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
-        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
-        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
-        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
+        let path = NSBundle.mainBundle().pathForResource("MainVCSettings.json", ofType: nil)
+        if let jsonPath = path {
+            let jsonData = NSData(contentsOfFile: jsonPath)
+            do{
+                let dictArr = try NSJSONSerialization.JSONObjectWithData(jsonData!, options: NSJSONReadingOptions.MutableContainers)
+                print(dictArr.count)
+                for dic in dictArr as! [[NSString : String]] {
+                    addChildViewController(dic["vcName"]!, title: dic["title"]!, imageName: dic["imageName"]!)
+                }
+            }catch{
+                print(error)
+                addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+                addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+                addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+                addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
+            }
+        }
     }
     
     private func addChildViewController(childControllerName: String,title:String,imageName:String) {
@@ -36,8 +49,5 @@ class MainViewController: UITabBarController {
         let nav = UINavigationController()
         nav.addChildViewController(vc)
         addChildViewController(nav)
-        
-        
-
     }
 }
