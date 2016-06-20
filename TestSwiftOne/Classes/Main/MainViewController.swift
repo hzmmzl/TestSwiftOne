30000dd5f2e2fced1cc69de10c0309152ee27cdb
mzl <you@example.com>
2016-06-20 17:43:45 +0800

    框架

--- TestSwiftOne/Classes/Main/MainViewController.swift
+++ TestSwiftOne/Classes/Main/MainViewController.swift
@@ -17,24 +17,27 @@ class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        tabBar.tintColor = UIColor.orangeColor()
+        
+        addChildViewController("HomeTableViewController", title: "首页", imageName: "tabbar_home")
+        addChildViewController("MessageTableViewController", title: "消息", imageName: "tabbar_message_center")
+        addChildViewController("DiscoverTableViewController", title: "广场", imageName: "tabbar_discover")
+        addChildViewController("ProfileTableViewController", title: "我", imageName: "tabbar_profile")
     }
     
+    private func addChildViewController(childControllerName: String,title:String,imageName:String) {
+        let ns = NSBundle.mainBundle().infoDictionary!["CFBundleExecutable"] as! String
+        let cls:AnyClass? = NSClassFromString(ns + "." + childControllerName)
+        let vcCls = cls as! UIViewController.Type
+        let vc = vcCls.init()
+        vc.tabBarItem.image = UIImage(named: imageName)
+        vc.tabBarItem.selectedImage = UIImage(named: imageName + "_highlighted")
+        vc.title = title
+        let nav = UINavigationController()
+        nav.addChildViewController(vc)
+        addChildViewController(nav)
+        
+        
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
-
 }
---
+++ TestSwiftOne/Classes/Main/Resource/NullViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NullViewController.swift
+//  XMGWeibo
+//
+//  Created by 李南江 on 15/8/31.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+import UIKit
+
+class NullViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
